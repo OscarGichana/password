2eabcc498483090968b48723d3116c234d7fd28b
Oscar Gichana <gichanaoscar3@gmail.com>
2021-02-15 16:46:39 +0300

    add save and delete methods and tests.

--- password.py
+++ password.py
@@ -20,4 +20,23 @@ class User:
         self.password = password
 
 
+#saving accounts
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
+
+
+  #delete
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+
+        User.user_list.remove(self)
+
 
--- password_test.py
+++ password_test.py
@@ -35,6 +35,46 @@ class TestContact(unittest.TestCase):
         self.assertEqual(self.new_user.password,"123")
 
 
+#saving contact
+    def test_save_user(self):
+        '''
+        test case to test if the user object is saved into the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)
+
+
+#adding 1 contact
+    def tearDown(self):
+            '''
+            method that does clean up after each test case has run.
+            '''
+            User.user_list = [] #creating empty list
+
+#adding multiple contacts
+    def test_save_multiple_user(self):
+            '''
+            to check if we can save multiple user objects to our contact_list
+            '''
+            self.new_user.save_user()
+            test_user = User("account","Test","user","username","0712345678","test@user.com","password") # new contact
+            test_user.save_user()
+            self.assertEqual(len(User.user_list),2)
+
+# delete
+    def test_delete_user(self):
+            '''
+            to test if we can remove a user from our user list
+            '''
+            self.new_user.save_user()
+            test_user = User("account","Test","user","username","0712345678","test@user.com","password")
+            test_user.save_user()
+
+            self.new_user.delete_user()# Deleting a contact object
+            self.assertEqual(len(User.user_list),1)
+
+
+
 
 
 if __name__ == '__main__':
