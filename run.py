5ccfb66d397c582a996a0713a5e1239234d4514f
Oscar Gichana <gichanaoscar3@gmail.com>
2021-02-15 17:04:16 +0300

    running the exit function.

--- run.py
+++ run.py
@@ -139,6 +139,11 @@ def main():
                           print("not found")
 
 
+          elif short_code == "ex":
+                  print("Bye .......")
+                  break
+          else:
+                  print("I really didn't get that. Please use the short codes")
 
 
 if __name__ == '__main__':
